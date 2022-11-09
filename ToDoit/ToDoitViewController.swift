377416b76d48686bbd92371a81a2cdfdca90c061
Jack-Camas <jackcamasdev@gmail.com>
2022-11-09 01:14:03 -0500

    Add TableView Cell Data

--- ToDoit/ToDoitViewController.swift
+++ ToDoit/ToDoitViewController.swift
@@ -9,13 +9,11 @@ import UIKit
 
 class ToDoitViewController: UITableViewController {
 
-    let itemArray = ["Find Mike", "Buy Eggos", "Destory Demorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destory Demorgon"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-    
-    //MARK - TablewView Datasource Methods
+    //MARK: TablewView Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -27,7 +25,38 @@ class ToDoitViewController: UITableViewController {
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
-
-
+    //MARK: TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = . checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    //MARK: Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { action in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create new Item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
+
+
 
