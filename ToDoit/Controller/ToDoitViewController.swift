c649f5ec272b3cfa1de4be416eb71ac59384682d
Jack-Camas <jackcamasdev@gmail.com>
2022-11-22 00:55:40 -0500

    Decode Data with NSCoder

--- ToDoit/Controller/ToDoitViewController.swift
+++ ToDoit/Controller/ToDoitViewController.swift
@@ -8,37 +8,18 @@
 import UIKit
 
 class ToDoitViewController: UITableViewController {
-
+    
     var itemArray = [Item]()
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     //let defaults = UserDefaults.standard
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
         print(dataFilePath)
-        
-        let newItem = Item()
-        newItem.title = "Test 1"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "Test 2"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Test 3"
-        itemArray.append(newItem3)
-        
-        
-//        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
-//            itemArray = items
-//        }
-        
+        loadItems()
     }
-    //MARK: - TablewView Datasource Methods 
+    //MARK: - TablewView Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -54,11 +35,11 @@ class ToDoitViewController: UITableViewController {
         //Ternary opeartor
         cell.accessoryType = item.done ? .checkmark : .none
         
-//        if item.done == true {
-//            cell.accessoryType = .checkmark
-//        } else {
-//            cell.accessoryType = .none
-//        }
+        //        if item.done == true {
+        //            cell.accessoryType = .checkmark
+        //        } else {
+        //            cell.accessoryType = .none
+        //        }
         return cell
     }
     //MARK: - TableView Delegate Methods
@@ -93,7 +74,7 @@ class ToDoitViewController: UITableViewController {
     }
     
     //MARK - Model Manipulation Methods
-
+    
     func saveItems() {
         
         let encoder = PropertyListEncoder()
@@ -104,6 +85,17 @@ class ToDoitViewController: UITableViewController {
             print("Error encoding item array, \(error)")
         }
         self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+        }
     }
 }
 
