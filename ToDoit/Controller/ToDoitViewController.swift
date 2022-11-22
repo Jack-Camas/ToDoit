5b9cf3a7657d144b3a794bf0f5d1bd6fd0a98f56
Jack-Camas <jackcamasdev@gmail.com>
2022-11-21 23:22:51 -0500

    Encode Data with NSCoder

--- ToDoit/Controller/ToDoitViewController.swift
+++ ToDoit/Controller/ToDoitViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 class ToDoitViewController: UITableViewController {
 
     var itemArray = [Item]()
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    //let defaults = UserDefaults.standard
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        print(dataFilePath)
         
         let newItem = Item()
         newItem.title = "Test 1"
@@ -28,7 +33,7 @@ class ToDoitViewController: UITableViewController {
         itemArray.append(newItem3)
         
         
-//        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+//        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
 //            itemArray = items
 //        }
         
@@ -61,7 +66,7 @@ class ToDoitViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        tableView.reloadData()
+        saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     //MARK: - Add New Items
@@ -72,11 +77,12 @@ class ToDoitViewController: UITableViewController {
         
         let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { action in
+            
             let newItem = Item()
             newItem.title = textField.text!
             self.itemArray.append(newItem)
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
-            self.tableView.reloadData()
+            self.saveItems()
+            
         }
         alert.addTextField { alertTextField in
             alertTextField.placeholder = "Create new Item"
@@ -84,6 +90,20 @@ class ToDoitViewController: UITableViewController {
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK - Model Manipulation Methods
+
+    func saveItems() {
+        
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        self.tableView.reloadData()
     }
 }
 
